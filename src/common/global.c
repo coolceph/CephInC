a58ca7c72e1d41c2006b903f583c09e4fd12fc88
Dong Yuan <yuandong1222@gmail.com>
2014-12-03 17:10:54 +0800

    add common/config and common/global
    Now we only have one option: client_type
    
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

---
+++ src/common/global.c
@@ -0,0 +1,4 @@
+#include "common/global.h"
+
+struct cceph_context *g_context = NULL;
+struct cceph_config  *g_conf    = NULL;
---
+++ src/common/global.h
@@ -0,0 +1,9 @@
+#ifndef CCEPH_GLOBAL_H
+#define CCEPH_GLOBAL_H
+
+#include "common/context.h"
+
+extern struct cceph_context *g_context;
+extern struct cceph_config *g_conf;
+
+#endif
