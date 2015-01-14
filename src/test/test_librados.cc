798da27541f3ad50dd092d5912a70c44041448de
Dong Yuan <yuandong1222@gmail.com>
2015-01-14 14:46:10 +0000

    Add test_librados.cc
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

---
+++ src/test/test_librados.cc
@@ -0,0 +1,12 @@
+#include "librados/librados.h"
+
+#include "gtest/gtest.h"
+
+//This is the unittest for librados
+//we should use null_client or mem_client to run this
+
+TEST(librados, Create) {
+  rados_t cluster;
+  int ret = rados_create(&cluster, "fff");
+  EXPECT_EQ(ret, 0);
+}
