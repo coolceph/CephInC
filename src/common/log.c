5cda3507a96195ac8411dd6a6409279a66f9b7ee
Dong Yuan <yuandong1222@gmail.com>
2015-12-21 19:40:27 +0800

    Func: add initial_log_id and new_log_id
    Log id is used to stand for a process such as a write req process or a
    recovery operation. The log id can be used to track such process.
    
    Log id will be initialized when a deamon(osd, mon, client) started by a
    prefix and a random num, the g_log_id will be set to prefix << 10 +
    random.
    
    Each process can use new_log_id() function to get a new log id.
    
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/common/log.c
+++ src/common/log.c
@@ -2,11 +2,17 @@
 
 #include "common/atomic.h"
 
-#include <math.h>
+#include <stdlib.h>
 
 static atomic64_t g_log_id = { 0 };
 
-void initial_log_id(int prefix) {
+void initial_log_id(int32_t prefix) {
+    srand((unsigned)time(0));
+
+    int64_t log_id = prefix;
+    log_id = (log_id + 10000000000) + rand();
+
+    atomic_set64(&g_log_id, log_id);
 }
 
 int64_t new_log_id() {
