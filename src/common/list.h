bba9ab1fc283b4caeba628275b96821d1a672ff2
Dong Yuan <yuandong1222@gmail.com>
2015-12-30 14:15:09 +0800

    Common: make list works with C++ for unit test
    new is a keyword in C++, use new_node instead.
    
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/common/list.h
+++ src/common/list.h
@@ -3,12 +3,12 @@
 
 #define POISON_POINTER_DELTA 0
 
-#define LIST_POISON1  ((void *) 0x00100100 + POISON_POINTER_DELTA)
-#define LIST_POISON2  ((void *) 0x00200200 + POISON_POINTER_DELTA)
+#define LIST_POISON1  ((char *) 0x00100100 + POISON_POINTER_DELTA)
+#define LIST_POISON2  ((char *) 0x00200200 + POISON_POINTER_DELTA)
 
 #define offsetof(type, member)  (size_t)(&((type*)0)->member)
 
-#define container_of(ptr, type, member) ({          \
+#define container_of(ptr, type, member) ({                   \
         const typeof(((type *)0)->member)*__mptr = (ptr);    \
     (type *)((char *)__mptr - offsetof(type, member)); })
 
@@ -22,51 +22,51 @@ static inline void init_list_head(struct list_head *list)
 {
     list->prev = list;
     list->next = list;
-}
+};
 
-static inline void __list_add(struct list_head *new,
+static inline void __list_add(struct list_head *new_node,
     struct list_head *prev, struct list_head *next)
 {
-    prev->next = new;
-    new->prev = prev;
-    new->next = next;
-    next->prev = new;
-}
+    prev->next = new_node;
+    new_node->prev = prev;
+    new_node->next = next;
+    next->prev = new_node;
+};
 
-static inline void list_add(struct list_head *new , struct list_head *head)
+static inline void list_add(struct list_head *new_node , struct list_head *head)
 {
-    __list_add(new, head, head->next);
-}
-static inline void list_add_tail(struct list_head *new, struct list_head *head)
+    __list_add(new_node, head, head->next);
+};
+static inline void list_add_tail(struct list_head *new_node, struct list_head *head)
 {
-    __list_add(new, head->prev, head);
-}
+    __list_add(new_node, head->prev, head);
+};
 
 static inline  void __list_del(struct list_head *prev, struct list_head *next)
 {
     prev->next = next;
     next->prev = prev;
-}
+};
 
 static inline void list_del(struct list_head *entry)
 {
     __list_del(entry->prev, entry->next);
-    entry->next = LIST_POISON1;
-    entry->prev = LIST_POISON2;
-}
+    entry->next = (struct list_head*)LIST_POISON1;
+    entry->prev = (struct list_head*)LIST_POISON2;
+};
 
 static inline void list_move(struct list_head *list, struct list_head *head)
 {
         __list_del(list->prev, list->next);
         list_add(list, head);
-}
+};
 
 static inline void list_move_tail(struct list_head *list,
                       struct list_head *head)
 {
         __list_del(list->prev, list->next);
         list_add_tail(list, head);
-}
+};
 #define list_entry(ptr, type, member) \
     container_of(ptr, type, member)
 
