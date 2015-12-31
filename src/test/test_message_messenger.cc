31cc0d21a947d18ea1515d5386499ebd1aa9f1cb
Dong Yuan <yuandong1222@gmail.com>
2015-12-31 14:47:07 +0800

    Test: Finish test for get_conn_by_fd
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/test/test_message_messenger.cc
+++ src/test/test_message_messenger.cc
@@ -10,4 +10,20 @@ TEST(message_messenger, find_conn_by_fd) {
     pthread_rwlock_init(&(handle.conn_list_lock), NULL);
 
     EXPECT_EQ(NULL, TEST_get_conn_by_fd(&handle, 0));
+
+    conn_t conn1;
+    conn1.host = (char*)"host1";
+    conn1.port = 9001;
+    conn1.fd   = 1;
+    list_add(&conn1.conn_list_node, &handle.conn_list.conn_list_node);
+
+    conn_t conn2;
+    conn2.host = (char*)"host2";
+    conn2.port = 9002;
+    conn2.fd   = 2;
+    list_add_tail(&conn2.conn_list_node, &handle.conn_list.conn_list_node);
+
+    EXPECT_EQ(&conn1, TEST_get_conn_by_fd(&handle, 1));
+    EXPECT_EQ(&conn2, TEST_get_conn_by_fd(&handle, 2));
+
 }
