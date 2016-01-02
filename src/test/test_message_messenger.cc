a71e5535010029f9a8cd1106a39c17f9e8813e39
Dong Yuan <yuandong1222@gmail.com>
2016-01-02 08:36:40 +0800

    Test: Finish test for find_conn_by_id
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/test/test_message_messenger.cc
+++ src/test/test_message_messenger.cc
@@ -13,9 +13,21 @@ conn_t* add_conn(msg_handle_t* handle, char* host, int port, int fd) {
     conn->host = host;
     conn->port = port;
     conn->fd   = fd;
+    conn->id   = fd + port;
     list_add(&conn->list_node, &handle->conn_list.list_node);
 
     return conn;
+}
+
+TEST(message_messenger, find_conn_by_id) {
+    msg_handle_t handle;
+    initial_msg_handle(&handle);
+    conn_t* conn1 = add_conn(&handle, (char*)"host1", 9001, 1);
+    conn_t* conn2 = add_conn(&handle, (char*)"host2", 9002, 2);
+
+    EXPECT_EQ(NULL, TEST_get_conn_by_id(&handle, 0));
+    EXPECT_EQ(conn1, TEST_get_conn_by_id(&handle, 9002));
+    EXPECT_EQ(conn2, TEST_get_conn_by_id(&handle, 9004));
 }
 
 TEST(message_messenger, find_conn_by_fd) {
