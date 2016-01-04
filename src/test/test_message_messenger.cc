4d3fa07c7e35e0984e8c5500d29ab52891b326fc
Dong Yuan <yuandong1222@gmail.com>
2016-01-04 19:55:50 +0800

    Test: refact test_message_messenger to remove inital_conn_list
    We use (TEST_)new_msg_handle instead.
    
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/test/test_message_messenger.cc
+++ src/test/test_message_messenger.cc
@@ -5,10 +5,6 @@ extern "C" {
 #include "bhook.h"
 #include "gtest/gtest.h"
 
-void initial_msg_handle(msg_handle_t* handle) {
-    init_list_head(&handle->conn_list.list_node);
-    pthread_rwlock_init(&handle->conn_list_lock, NULL);
-}
 conn_t* add_conn(msg_handle_t* handle, char* host, int port, int fd) {
     conn_t* conn = (conn_t*)malloc(sizeof(conn_t));
     conn->host = host;
@@ -45,34 +41,31 @@ TEST(message_messenger, new_msg_handle) {
 }
 
 TEST(message_messenger, find_conn_by_id) {
-    msg_handle_t handle;
-    initial_msg_handle(&handle);
-    conn_t* conn1 = add_conn(&handle, (char*)"host1", 9001, 1);
-    conn_t* conn2 = add_conn(&handle, (char*)"host2", 9002, 2);
+    msg_handle_t* handle = TEST_new_msg_handle(&mock_process_message, 1);
+    conn_t* conn1 = add_conn(handle, (char*)"host1", 9001, 1);
+    conn_t* conn2 = add_conn(handle, (char*)"host2", 9002, 2);
 
-    EXPECT_EQ(NULL, TEST_get_conn_by_id(&handle, 0));
-    EXPECT_EQ(conn1, TEST_get_conn_by_id(&handle, 9002));
-    EXPECT_EQ(conn2, TEST_get_conn_by_id(&handle, 9004));
+    EXPECT_EQ(NULL, TEST_get_conn_by_id(handle, 0));
+    EXPECT_EQ(conn1, TEST_get_conn_by_id(handle, 9002));
+    EXPECT_EQ(conn2, TEST_get_conn_by_id(handle, 9004));
 }
 
 TEST(message_messenger, find_conn_by_fd) {
-    msg_handle_t handle;
-    initial_msg_handle(&handle);
-    conn_t* conn1 = add_conn(&handle, (char*)"host1", 9001, 1);
-    conn_t* conn2 = add_conn(&handle, (char*)"host2", 9002, 2);
+    msg_handle_t* handle = TEST_new_msg_handle(&mock_process_message, 1);
+    conn_t* conn1 = add_conn(handle, (char*)"host1", 9001, 1);
+    conn_t* conn2 = add_conn(handle, (char*)"host2", 9002, 2);
 
-    EXPECT_EQ(NULL, TEST_get_conn_by_fd(&handle, 0));
-    EXPECT_EQ(conn1, TEST_get_conn_by_fd(&handle, 1));
-    EXPECT_EQ(conn2, TEST_get_conn_by_fd(&handle, 2));
+    EXPECT_EQ(NULL, TEST_get_conn_by_fd(handle, 0));
+    EXPECT_EQ(conn1, TEST_get_conn_by_fd(handle, 1));
+    EXPECT_EQ(conn2, TEST_get_conn_by_fd(handle, 2));
 }
 
 TEST(message_messenger, find_conn_by_port_and_ip) {
-    msg_handle_t handle;
-    initial_msg_handle(&handle);
-    conn_t* conn1 = add_conn(&handle, (char*)"host1", 9001, 1);
-    conn_t* conn2 = add_conn(&handle, (char*)"host2", 9002, 2);
+    msg_handle_t* handle = TEST_new_msg_handle(&mock_process_message, 1);
+    conn_t* conn1 = add_conn(handle, (char*)"host1", 9001, 1);
+    conn_t* conn2 = add_conn(handle, (char*)"host2", 9002, 2);
 
-    EXPECT_EQ(NULL, TEST_get_conn_by_host_and_port(&handle, (char*)"no_this_host", 1));
-    EXPECT_EQ(conn1, TEST_get_conn_by_host_and_port(&handle, (char*)"host1", 9001));
-    EXPECT_EQ(conn2, TEST_get_conn_by_host_and_port(&handle, (char*)"host2", 9002));
+    EXPECT_EQ(NULL, TEST_get_conn_by_host_and_port(handle, (char*)"no_this_host", 1));
+    EXPECT_EQ(conn1, TEST_get_conn_by_host_and_port(handle, (char*)"host1", 9001));
+    EXPECT_EQ(conn2, TEST_get_conn_by_host_and_port(handle, (char*)"host2", 9002));
 }
