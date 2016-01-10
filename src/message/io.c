961835ee9b255985db2b81fc56a3a20bf0b5c7bf
Dong Yuan <yuandong1222@gmail.com>
2016-01-10 19:43:05 +0800

    Msg: read_* will return errno when read failed such as conn closed
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/message/io.c
+++ src/message/io.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <errno.h>
 
+#include "include/errno.h"
+
 static int read_from_conn(int data_fd, void* buf, size_t size, int64_t log_id);
 
 int send_int8(int fd, int8_t value, int64_t log_id) {
@@ -61,50 +63,48 @@ int send_data(int fd, int64_t length, char* data, int64_t log_id) {
 }
 
 int read_int8(int data_fd, int8_t* value, int64_t log_id) {
-    return read_from_conn(data_fd, value, sizeof(*value), log_id) == sizeof(*value) ? 0 : -1;
+    int ret = read_from_conn(data_fd, value, sizeof(int8_t), log_id);
+    return (ret == sizeof(int8_t)) ? 0 : (ret < 0 ? ret : -1);
 }
 int read_int64(int data_fd, int64_t* value, int64_t log_id) {
-    return read_from_conn(data_fd, value, sizeof(*value), log_id) == sizeof(*value) ? 0 : -1;
+    int ret = read_from_conn(data_fd, value, sizeof(int64_t), log_id);
+    return (ret == sizeof(int64_t)) ? 0 : (ret < 0 ? ret : -1);
 }
 int read_string(int data_fd, int16_t *size, char **string, int64_t log_id) {
-    if(read_from_conn(data_fd, size, sizeof(*size), log_id) != sizeof(*size)) {
-        return -1;
-    }
+    int ret = read_from_conn(data_fd, size, sizeof(int16_t), log_id);
+    return (ret == sizeof(int16_t)) ? 0 : (ret < 0 ? ret : -1);
     
     *string = malloc(*size + 1);
     (*string)[*size] = '\0';
-    if (read_from_conn(data_fd, *string, *size, log_id) != *size) {
+    ret = read_from_conn(data_fd, *string, *size, log_id);
+    if (ret < 0 || ret != *size) {
         free(*string);
         *string = NULL;
-        return -1;
     }
-    return 0;
+    return (ret == *size) ? 0 : (ret < 0 ? ret : -1);
 }
 int read_data(int data_fd, int64_t *size, char **data, int64_t log_id) {
-    if(read_from_conn(data_fd, size, sizeof(*size), log_id) != sizeof(*size)) {
-        return -1;
-    }
+    int ret = read_from_conn(data_fd, size, sizeof(*size), log_id);
+    return (ret == sizeof(int64_t)) ? 0 : (ret < 0 ? ret : -1);
     
     *data = malloc(*size);
-    if (read_from_conn(data_fd, *data, *size, log_id) != *size) {
+    ret = read_from_conn(data_fd, *data, *size, log_id);
+    if (ret < 0 || ret != *size) {
         free(*data);
         *data = NULL;
-        return -1;
     }
-    return 0;
+    return (ret == *size) ? 0 : (ret < 0 ? ret : -1);
 }
 
 static int read_from_conn(int data_fd, void* buf, size_t size, int64_t log_id) {
     int total = 0;
-    int closed = 0;
     while(size > 0) {
         int count = recv(data_fd, buf, size, 0);
         if (count == -1 && errno != EAGAIN) {
             /* If errno == EAGAIN, that means we have read all
                data. So go back to the main loop. */
             LOG(LL_ERROR, log_id, "read close");
-            closed = 1;
-            break;
+            return CCEPH_ERR_CONN_CLOSED; //messenger will close it
         }
 
         if (count == -1 && errno == EAGAIN && size > 0) {
@@ -116,16 +116,12 @@ static int read_from_conn(int data_fd, void* buf, size_t size, int64_t log_id) {
         if (count == 0) {
             if (size > 0) LOG(LL_ERROR, log_id, "incomplete read");
             LOG(LL_ERROR, log_id, "read close by ret 0");
-            closed = 1;
-            break;
+            return CCEPH_ERR_CONN_CLOSED; //messenger will close it
         }
         
         buf  += count;
         size -= count;
         total += count;
-    }
-    if (closed) {
-        LOG(LL_INFO, log_id, "Closed connection on descriptor %d", data_fd);
     }
     return total;
 }
