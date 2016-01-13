a9a399306c024b74b1477e8bf0bf8913607660f9
Dong Yuan <yuandong1222@gmail.com>
2016-01-13 10:08:58 +0800

    Msg: Finsh msg_header functions
    Include recv_msg_header, send_msg_header and free_msg_header.
    
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/message/msg_header.c
+++ src/message/msg_header.c
@@ -1,15 +1,36 @@
 #include "message/msg_header.h"
 
+#include <unistd.h>
+
+#include "common/assert.h"
+#include "message/io.h"
+
 extern int recv_msg_header(int fd, msg_header* header, int64_t log_id) {
-    //TODO: mv to other file
-    //int8_t op;
-    //if(read_int8(fd, &op, log_id) != 0) return NULL;
+    assert(log_id, header == NULL);
+
+    int ret = recv_int8(fd, &header->op, log_id);
+    if (ret != 0) return ret;
+
+    ret = recv_int64(fd, &header->log_id, log_id);
+    if (ret != 0) return ret;
+
     return 0;
 }
 extern int send_msg_header(int fd, msg_header* header, int64_t log_id) {
+    assert(log_id, header == NULL);
+
+    int ret = send_int8(fd, header->op, log_id);
+    if (ret != 0) return ret;
+
+    ret = send_int64(fd, header->log_id, log_id);
+    if (ret != 0) return ret;
+
     return 0;
 }
 extern int free_msg_header(msg_header** header) {
-    return 0;
+    assert(log_id, *header == NULL);
+
+    free(*header);
+    *header = NULL;
 }
 
