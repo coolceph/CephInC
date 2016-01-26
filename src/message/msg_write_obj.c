ff0155a9e3f806823ead056fb9c958e598c369ff
Dong Yuan <yuandong1222@gmail.com>
2016-01-26 14:29:26 +0800

    Msg: Fix small bug for send_msg_write_obj_ack
    The test can be passed now, but we still need more EXPECT
    
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/message/msg_write_obj.c
+++ src/message/msg_write_obj.c
@@ -86,6 +86,6 @@ extern int send_msg_write_obj_ack(int fd, msg_write_obj_ack* msg, int64_t log_id
     int ret = 0;
     CCEPH_SEND_FIELD(client_id, int32, msg->client_id);
     CCEPH_SEND_FIELD(req_id, int32, msg->req_id);
-    CCEPH_RECV_FIELD(result, int8, &msg->result);
+    CCEPH_SEND_FIELD(result, int8, msg->result);
     return 0;
 }
