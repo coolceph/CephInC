c0dcee9617fe0bab62d2d962b9cb20e4113766ee
Dong Yuan <yuandong1222@gmail.com>
2016-02-09 21:11:17 +0800

    OSD: Refact cceph_osd::main by server_messenger
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/cceph_osd.c
+++ src/cceph_osd.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/epoll.h>
@@ -16,88 +14,9 @@
 
 #include "message/io.h"
 #include "message/messenger.h"
+#include "message/server_messenger.h"
 #include "message/msg_header.h"
 #include "message/msg_write_obj.h"
-
-#define MAXEVENTS 64
-
-static int make_socket_non_blocking(int socket_fd, int64_t log_id) {
-    int flags;
-
-    flags = fcntl(socket_fd, F_GETFL, 0);
-    if (flags == -1) {
-       LOG(LL_ERROR, log_id, "fcntl");
-       return -1;
-    }
-
-    flags |= O_NONBLOCK;
-    if (fcntl(socket_fd, F_SETFL, flags) == -1) {
-        LOG(LL_ERROR, log_id, "fcntl");
-        return -1;
-    }
-    return 0;
-}
-
-static int create_and_bind(char *port, int64_t log_id) {
-    struct addrinfo hints;
-    struct addrinfo *result, *rp;
-    int ret, socket_fd;
-
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;     /* Return IPv4 and IPv6 choices */
-    hints.ai_socktype = SOCK_STREAM; /* We want a TCP socket */
-    hints.ai_flags = AI_PASSIVE;     /* All interfaces */
-
-    ret = getaddrinfo(NULL, port, &hints, &result);
-    if (ret != 0) {
-        LOG(LL_ERROR, log_id, "getaddrinfo: %s", gai_strerror(ret));
-        return -1;
-    }
-
-    for (rp = result; rp != NULL; rp = rp->ai_next) {
-        socket_fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        if (socket_fd == -1)  continue;
-
-        ret = bind(socket_fd, rp->ai_addr, rp->ai_addrlen);
-        if (ret == 0) break;
-
-        close(socket_fd);
-    }
-
-    if (rp == NULL) {
-        LOG(LL_ERROR, log_id, "Could not bind");
-        return -1;
-    }
-
-    freeaddrinfo(result);
-    return socket_fd;
-}
-
-static int new_connection(int socket_fd, msg_handle* msg_handle, int64_t log_id) {
-    int ret;
-    
-    struct sockaddr in_addr;
-    socklen_t in_len = sizeof(in_addr);
-    int infd = accept(socket_fd, &in_addr, &in_len);
-    if (infd == -1 && errno != EAGAIN && errno != EWOULDBLOCK) {
-        LOG(LL_ERROR, log_id, "Accept error.");
-        return -1;
-    }
-    
-    char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
-    ret = getnameinfo(&in_addr, in_len, 
-                      hbuf, sizeof(hbuf), sbuf, sizeof(sbuf),
-                      NI_NUMERICHOST | NI_NUMERICSERV);
-    if (ret == 0) {
-        LOG(LL_INFO, log_id, "Accepted connection on descriptor %d "
-                             "(host=%s, port=%s).", infd, hbuf, sbuf);
-    }
-
-    conn_id_t conn_id = new_conn(msg_handle, hbuf, atoi(sbuf), infd, log_id);
-    return conn_id < 0 ? conn_id : 0;
-}
-
-
 
 static void do_req_write(msg_write_obj_req* req) {
     char data_dir[] = "./data";
@@ -134,62 +53,6 @@ static int process_message(msg_handle* msg_handle, conn_id_t conn_id, msg_header
     return 0;
 }
 
-static int is_conn_err(struct epoll_event event) {
-    return (event.events & EPOLLERR) 
-           || (event.events & EPOLLHUP) 
-           || !(event.events & EPOLLIN);
-}
-
-static int start_server(char* port, msg_handle* msg_handle, int64_t log_id) {
-    int ret;
-
-    int socket_fd = create_and_bind(port, log_id);
-    if (socket_fd == -1) abort();
-
-    ret = make_socket_non_blocking(socket_fd, log_id);
-    if (ret == -1) abort();
-
-    ret = listen(socket_fd, SOMAXCONN);
-    if (ret == -1) {
-        LOG(LL_ERROR, log_id, "listen");
-        abort();
-    }
-
-    int epoll_fd = epoll_create1(0);
-    if (epoll_fd == -1) {
-        LOG(LL_ERROR, log_id, "epoll_create");
-        abort();
-    }
-
-    struct epoll_event event;
-    event.data.fd = socket_fd;
-    event.events = EPOLLIN | EPOLLET;
-    ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket_fd, &event);
-    if (ret == -1) {
-        LOG(LL_ERROR, log_id, "epoll_ctl");
-        abort();
-    }
-
-    while (1) {
-        int fd_count = epoll_wait(epoll_fd, &event, 1, -1);
-        if (fd_count <= 0) {
-            //TODO: error log here
-            continue;
-        }
-
-        int fd = event.data.fd;
-        assert(log_id, fd == socket_fd);
-        if (is_conn_err(event)) {
-            LOG(LL_FATAL, log_id, "Listen Socked Closed.");
-            close(fd);
-            abort();
-        }
-
-        new_connection(socket_fd, msg_handle, log_id);
-    }
-
-    close(socket_fd);
-}
 
 int main(int argc, char *argv[]) {
 
@@ -197,17 +60,17 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s [port]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    char* port = argv[1];
+    int port = atoi(argv[1]);
 
     int32_t log_prefix = 201;
     initial_log_id(log_prefix);
 
-
     int64_t log_id = new_log_id();
     msg_handle* msg_handle = new_msg_handle(&process_message, NULL, log_id);
-    start_messager(msg_handle, log_id);
+    server_msg_handle *server_msg_handle = new_server_msg_handle(msg_handle, port, log_id);
 
-    start_server(port, msg_handle, new_log_id());
+    start_server_messenger(server_msg_handle, log_id);
+
     return 0;
 }
 
