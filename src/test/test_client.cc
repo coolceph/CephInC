d0199df853680745aca7825327f5bd2e15c3a20e
Dong Yuan <yuandong1222@gmail.com>
2016-02-23 17:59:39 +0800

    Client: Standardize client_handle funcs
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/test/test_client.cc
+++ src/test/test_client.cc
@@ -6,7 +6,7 @@ extern "C" {
 
 TEST(client, new_client_handle) {
     cceph_osdmap osdmap;
-    cceph_client_handle *handle = cceph_new_client_handle(&osdmap);
+    cceph_client_handle *handle = cceph_client_handle_new(&osdmap);
     EXPECT_NE((cceph_client_handle*)NULL, handle);
     EXPECT_NE((msg_handle*)NULL, handle->msg_handle);
     EXPECT_EQ(&osdmap, handle->osdmap);
@@ -14,9 +14,9 @@ TEST(client, new_client_handle) {
 }
 TEST(client, initial_client) {
     cceph_osdmap osdmap;
-    cceph_client_handle *handle = cceph_new_client_handle(&osdmap);
+    cceph_client_handle *handle = cceph_client_handle_new(&osdmap);
 
-    int ret = cceph_initial_client(handle);
+    int ret = cceph_client_initial(handle);
     EXPECT_EQ(0, ret);
     EXPECT_EQ(CCEPH_CLIENT_STATE_NORMAL, handle->state);
 }
