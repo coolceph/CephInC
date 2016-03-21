e5487dd110a61c3be09c9f29cfccc11511b47fe3
Dong Yuan <yuandong1222@gmail.com>
2016-03-21 14:13:05 +0800

    OS: add mem_store.h/c
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

---
+++ src/os/mem_store.h
@@ -0,0 +1,26 @@
+#ifndef CCEPH_MEM_STORE_H
+#define CCEPH_MEM_STORE_H
+
+#include "os/object_store.h"
+
+typedef struct {
+} cceph_mem_store;
+
+extern cceph_mem_store* cceph_mem_store_new(const char* path);
+
+
+extern int cceph_mem_store_mount(
+        cceph_object_store* os);
+
+extern int cceph_mem_store_queue_transaction(
+        cceph_object_store* os,
+        cceph_os_transaction transaction);
+
+extern int cceph_mem_store_read_object(
+        cceph_object_store* os,
+        const char*         oid,
+        int64_t             offset,
+        int64_t             length,
+        char*               data);
+
+#endif
