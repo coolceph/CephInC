5078635df4a4874d2c066eac2ba746dfbc9fec5e
Dong Yuan <yuandong1222@gmail.com>
2016-03-24 19:43:47 +0800

    OS: small improvement of ObjectStore and MemStore
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/os/mem_store.h
+++ src/os/mem_store.h
@@ -3,24 +3,27 @@
 
 #include "os/object_store.h"
 
-typedef struct {
-} cceph_mem_store;
+typedef void* cceph_mem_store;
+
+extern cceph_os_funcs* cceph_mem_store_get_funcs();
 
 extern cceph_mem_store* cceph_mem_store_new();
 
 extern int cceph_mem_store_mount(
-        cceph_object_store* os,
-        int64_t log_id);
+        cceph_object_store*  os,
+        int64_t              log_id);
 
 extern int cceph_mem_store_submit_transaction(
-        cceph_object_store* os,
-        cceph_os_transaction transaction);
+        cceph_object_store*  os,
+        cceph_os_transaction transaction,
+        int64_t              log_id);
 
 extern int cceph_mem_store_read_object(
-        cceph_object_store* os,
-        const char*         oid,
-        int64_t             offset,
-        int64_t             length,
-        char*               data);
+        cceph_object_store*  os,
+        const char*          oid,
+        int64_t              offset,
+        int64_t              length,
+        char*                data,
+        int64_t              log_id);
 
 #endif
--- src/os/object_store.h
+++ src/os/object_store.h
@@ -7,23 +7,26 @@
 typedef void* cceph_object_store;
 
 typedef int (*cceph_os_mount_func)(
-        cceph_object_store* os);
+        cceph_object_store* os,
+        int64_t             log_id);
 
 typedef int (*cceph_os_submit_transaction_func)(
-        cceph_object_store* os,
-        cceph_os_transaction transaction);
+        cceph_object_store*  os,
+        cceph_os_transaction transaction,
+        int64_t              log_id);
 
 typedef int (*cceph_os_read_object_func)(
-        cceph_object_store* os,
-        const char*         oid,
-        int64_t             offset,
-        int64_t             length,
-        char*               data);
+        cceph_object_store*  os,
+        const char*          oid,
+        int64_t              offset,
+        int64_t              length,
+        char*                data,
+        int64_t              log_id);
 
 typedef struct {
-    cceph_os_mount_func             mount;
-    cceph_os_submit_transaction_func queue_transaction;
-    cceph_os_read_object_func       read;
+    cceph_os_mount_func              mount;
+    cceph_os_submit_transaction_func submit_transaction;
+    cceph_os_read_object_func        read;
 } cceph_os_funcs;
 
 #endif
