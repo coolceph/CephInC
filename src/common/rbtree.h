f1e3c495b7bd227cbd60479ff7fee750fe8d169e
Dong Yuan <yuandong1222@gmail.com>
2016-03-29 20:34:03 +0800

    Common: add cceph prefix to rbtree.h
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/common/rbtree.h
+++ src/common/rbtree.h
@@ -34,7 +34,7 @@
 static inline struct page * rb_search_page_cache(struct inode * inode,
 						 unsigned long offset)
 {
-	struct rb_node * n = inode->i_rb_page_cache.rb_node;
+	cceph_rb_node * n = inode->i_rb_page_cache.rb_node;
 	struct page * page;
 
 	while (n)
@@ -53,10 +53,10 @@ static inline struct page * rb_search_page_cache(struct inode * inode,
 
 static inline struct page * __rb_insert_page_cache(struct inode * inode,
 						   unsigned long offset,
-						   struct rb_node * node)
+						   cceph_rb_node * node)
 {
-	struct rb_node ** p = &inode->i_rb_page_cache.rb_node;
-	struct rb_node * parent = NULL;
+	cceph_rb_node ** p = &inode->i_rb_page_cache.rb_node;
+	cceph_rb_node * parent = NULL;
 	struct page * page;
 
 	while (*p)
@@ -79,7 +79,7 @@ static inline struct page * __rb_insert_page_cache(struct inode * inode,
 
 static inline struct page * rb_insert_page_cache(struct inode * inode,
 						 unsigned long offset,
-						 struct rb_node * node)
+						 cceph_rb_node * node)
 {
 	struct page * ret;
 	if ((ret = __rb_insert_page_cache(inode, offset, node)))
@@ -97,57 +97,59 @@ static inline struct page * rb_insert_page_cache(struct inode * inode,
 #define	CCEPH_RB_RED    0
 #define	CCEPH_RB_BLACK  1
 
-struct rb_node
-{
+typedef struct cceph_rb_node_ cceph_rb_node_;
+struct cceph_rb_node_ {
 	unsigned long  rb_parent_color;
-	struct rb_node *rb_right;
-	struct rb_node *rb_left;
+	struct cceph_rb_node_ *rb_right;
+	struct cceph_rb_node_ *rb_left;
 } __attribute__((aligned(sizeof(long))));
+typedef struct cceph_rb_node_ cceph_rb_node;
 
 typedef struct {
-	struct rb_node *rb_node;
+	cceph_rb_node *rb_node;
 } cceph_rb_root;
 
+#define cceph_rb_parent(r)     ((cceph_rb_node *)((r)->rb_parent_color & ~3))
+#define cceph_rb_color(r)      ((r)->rb_parent_color & 1)
+#define cceph_rb_is_red(r)     (!rb_color(r))
+#define cceph_rb_is_black(r)   cceph_rb_color(r)
+#define cceph_rb_set_red(r)    do { (r)->rb_parent_color &= ~1; } while (0)
+#define cceph_rb_set_black(r)  do { (r)->rb_parent_color |= 1; } while (0)
 
-#define rb_parent(r)   ((struct rb_node *)((r)->rb_parent_color & ~3))
-#define rb_color(r)   ((r)->rb_parent_color & 1)
-#define rb_is_red(r)   (!rb_color(r))
-#define rb_is_black(r) rb_color(r)
-#define rb_set_red(r)  do { (r)->rb_parent_color &= ~1; } while (0)
-#define rb_set_black(r)  do { (r)->rb_parent_color |= 1; } while (0)
+#define	cceph_rb_entry(ptr, type, member) container_of(ptr, type, member)
 
-static inline void rb_set_parent(struct rb_node *rb, struct rb_node *p)
-{
+static inline void cceph_rb_set_parent(cceph_rb_node *rb, cceph_rb_node *p) {
 	rb->rb_parent_color = (rb->rb_parent_color & 3) | (unsigned long)p;
 }
-static inline void rb_set_color(struct rb_node *rb, int color)
-{
+static inline void cceph_rb_set_color(cceph_rb_node *rb, int color) {
 	rb->rb_parent_color = (rb->rb_parent_color & ~1) | color;
 }
 
-#define RB_ROOT	(struct rb_root) { NULL, }
-#define	rb_entry(ptr, type, member) container_of(ptr, type, member)
+#define CCEPH_RB_ROOT               (cceph_rb_root) { NULL, }
+#define CCEPH_RB_EMPTY_ROOT(root)   ((root)->rb_node == NULL)
+#define CCEPH_RB_EMPTY_NODE(node)   (cceph_rb_parent(node) == node)
+#define CCEPH_RB_CLEAR_NODE(node)   (cceph_rb_set_parent(node, node))
 
-#define RB_EMPTY_ROOT(root)	((root)->rb_node == NULL)
-#define RB_EMPTY_NODE(node)	(rb_parent(node) == node)
-#define RB_CLEAR_NODE(node)	(rb_set_parent(node, node))
-
-extern void rb_insert_color(struct rb_node *, struct rb_root *);
-extern void rb_erase(struct rb_node *, struct rb_root *);
+extern void cceph_rb_insert_color(cceph_rb_node *, cceph_rb_root *);
+extern void cceph_rb_erase(cceph_rb_node *, cceph_rb_root *);
 
 /* Find logical next and previous nodes in a tree */
-extern struct rb_node *rb_next(const struct rb_node *);
-extern struct rb_node *rb_prev(const struct rb_node *);
-extern struct rb_node *rb_first(const struct rb_root *);
-extern struct rb_node *rb_last(const struct rb_root *);
+extern cceph_rb_node *cceph_rb_next(const cceph_rb_node *);
+extern cceph_rb_node *cceph_rb_prev(const cceph_rb_node *);
+extern cceph_rb_node *cceph_rb_first(const cceph_rb_root *);
+extern cceph_rb_node *cceph_rb_last(const cceph_rb_root *);
 
 /* Fast replacement of a single node without remove/rebalance/add/rebalance */
-extern void rb_replace_node(struct rb_node *victim, struct rb_node *new,
-			    struct rb_root *root);
+extern void cceph_rb_replace_node(
+        cceph_rb_node *victim,
+        cceph_rb_node *new,
+        cceph_rb_root *root);
 
-static inline void rb_link_node(struct rb_node * node, struct rb_node * parent,
-				struct rb_node ** rb_link)
-{
+static inline void cceph_rb_link_node(
+        cceph_rb_node *node,
+        cceph_rb_node *parent,
+        cceph_rb_node **rb_link) {
+
 	node->rb_parent_color = (unsigned long )parent;
 	node->rb_left = node->rb_right = NULL;
 
