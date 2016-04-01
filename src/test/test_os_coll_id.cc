226edeb604ca54bd746ea17a962dfd05254072d6
Dong Yuan <yuandong1222@gmail.com>
2016-04-01 16:47:04 +0800

    OS: Add test for cceph_os_coll_id_cmp
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

---
+++ src/test/test_os_coll_id.cc
@@ -0,0 +1,11 @@
+extern "C" {
+#include "os/coll_id.h"
+}
+
+#include "gtest/gtest.h"
+
+TEST(os_coll_id, cmp) {
+    EXPECT_EQ(-1, cceph_os_coll_id_cmp(1, 2));
+    EXPECT_EQ(1, cceph_os_coll_id_cmp(3, 2));
+    EXPECT_EQ(0, cceph_os_coll_id_cmp(2, 2));
+}
