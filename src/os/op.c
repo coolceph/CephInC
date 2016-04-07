423488531b3c043121c93058e25b4b44470ac9bd
Dong Yuan <yuandong1222@gmail.com>
2016-04-07 15:25:29 +0800

    OS: Add some new ops
    Including TouchObject, RemoveObject, CreateCollection, RemoveCollection
    
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/os/op.c
+++ src/os/op.c
@@ -3,11 +3,19 @@
 const char* cceph_os_op_to_str(int op) {
     switch(op) {
         case CCEPH_OS_OP_NOOP:
-            return "noop";
+            return "NoOp";
         case CCEPH_OS_OP_WRITE:
-            return "write";
+            return "WriteObject";
+        case CCEPH_OS_OP_TOUCH:
+            return "TouchObject";
+        case CCEPH_OS_OP_REMOVE:
+            return "RemoveObject";
+        case CCEPH_OS_OP_CREATE_COLL:
+            return "CreateCollection";
+        case CCEPH_OS_OP_REMOVE_COLL:
+            return "RemoveCollection";
         default:
-            return "unkown_op";
+            return "UnknownOp";
     }
 
 }
--- src/os/op.h
+++ src/os/op.h
@@ -5,8 +5,12 @@
 
 typedef int32_t cceph_os_op_t;
 
-#define CCEPH_OS_OP_NOOP    0
-#define CCEPH_OS_OP_WRITE   1
+#define CCEPH_OS_OP_NOOP          0
+#define CCEPH_OS_OP_WRITE         1
+#define CCEPH_OS_OP_TOUCH         2
+#define CCEPH_OS_OP_REMOVE        3
+#define CCEPH_OS_OP_CREATE_COLL   4
+#define CCEPH_OS_OP_REMOVE_COLL   5
 
 extern const char* cceph_os_op_to_str(int op);
 
