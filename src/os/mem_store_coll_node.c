242895900fb7ac8a9aa088e61b258a4404239c6e
Dong Yuan <yuandong1222@gmail.com>
2016-04-11 22:25:29 +0800

    OS: Add mem_store_coll_node new/free funcs
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/os/mem_store_coll_node.c
+++ src/os/mem_store_coll_node.c
@@ -4,6 +4,36 @@
 #include "common/errno.h"
 #include "os/mem_store_coll_node.h"
 
+extern int cceph_mem_store_coll_node_new(
+        cceph_os_coll_id_t          cid,
+        cceph_mem_store_coll_node** node,
+        int64_t                     log_id) {
+    assert(log_id, node != NULL);
+    assert(log_id, *node == NULL);
+    assert(log_id, cid >= 0);
+
+    *node = (cceph_mem_store_coll_node*)malloc(sizeof(cceph_mem_store_coll_node));
+    if (*node == NULL) {
+        return CCEPH_ERR_NO_ENOUGH_MEM;
+    }
+
+    (*node)->cid = cid;
+    (*node)->objects = CCEPH_RB_ROOT;
+
+    return CCEPH_OK;
+}
+
+extern int cceph_mem_store_coll_node_free(
+        cceph_mem_store_coll_node** node,
+        int64_t                     log_id) {
+    assert(log_id, node != NULL);
+    assert(log_id, *node != NULL);
+
+    free(*node);
+    *node = NULL;
+    return CCEPH_OK;
+}
+
 int cceph_mem_store_coll_node_search(
         cceph_rb_root*              root,
         cceph_os_coll_id_t          cid,
--- src/os/mem_store_coll_node.h
+++ src/os/mem_store_coll_node.h
@@ -11,6 +11,15 @@ typedef struct {
     cceph_rb_node node;
 } cceph_mem_store_coll_node;
 
+extern int cceph_mem_store_coll_node_new(
+        cceph_os_coll_id_t          cid,
+        cceph_mem_store_coll_node** node,
+        int64_t                     log_id);
+
+extern int cceph_mem_store_coll_node_free(
+        cceph_mem_store_coll_node** node,
+        int64_t                     log_id);
+
 extern int cceph_mem_store_coll_node_search(
         cceph_rb_root*              root,
         cceph_os_coll_id_t          cid,
