e43cfee697f573f555c0c6c08d5d1f7aacbc2133
Dong Yuan <yuandong1222@gmail.com>
2016-04-15 16:41:04 +0800

    MemStore: Extract Coll Test to an Individual File
    We need more test cases for CollNode
    
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/test/test_os_mem_store.cc
+++ src/test/test_os_mem_store.cc
@@ -5,23 +5,6 @@ extern "C" {
 
 #include "gtest/gtest.h"
 
-TEST(os_mem_store, cceph_mem_store_coll_node) {
-    cceph_rb_root root = CCEPH_RB_ROOT;
-    for (int i = 0; i < 1000; i++) {
-        cceph_mem_store_coll_node *node = new cceph_mem_store_coll_node();
-        node->cid = i;
-        int ret = cceph_mem_store_coll_node_insert(&root, node, 0);
-        EXPECT_EQ(CCEPH_OK, ret);
-    }
-    for (int i = 0; i < 1000; i++) {
-        cceph_mem_store_coll_node *node = NULL;
-        int ret = cceph_mem_store_coll_node_search(&root, i, &node, 0);
-        EXPECT_EQ(CCEPH_OK, ret);
-        EXPECT_NE((cceph_mem_store_coll_node*)NULL, node);
-        EXPECT_EQ(i, node->cid);
-    }
-}
-
 TEST(os_mem_store, cceph_mem_store_object_node) {
     cceph_rb_root root = CCEPH_RB_ROOT;
     char oid[256];
