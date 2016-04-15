cb494b8389ecfe8a734eaa95a836ec931bcd8fb6
Dong Yuan <yuandong1222@gmail.com>
2016-04-15 17:04:14 +0800

    MemStore: Extract Object Test to an Individual File
    We need more test cases for ObjectNode
    
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

---
+++ src/test/test_os_mem_store_object_node.cc
@@ -0,0 +1,36 @@
+extern "C" {
+#include "common/errno.h"
+#include "os/mem_store_object_node.h"
+}
+
+#include "gtest/gtest.h"
+
+TEST(os_mem_store, cceph_mem_store_object_node) {
+    cceph_rb_root root = CCEPH_RB_ROOT;
+    char oid[256];
+    for (int i = 0; i < 1000; i++) {
+        bzero(oid, 256);
+        sprintf(oid, "%d", i);
+
+        cceph_mem_store_object_node *node = NULL;
+        int ret = cceph_mem_store_object_node_new(oid, &node, 0);
+        EXPECT_EQ(CCEPH_OK, ret);
+        EXPECT_STREQ(oid, node->oid);
+        EXPECT_EQ(NULL, node->data);
+        EXPECT_EQ(0, node->length);
+
+        ret = cceph_mem_store_object_node_insert(&root, node, 0);
+        EXPECT_EQ(CCEPH_OK, ret);
+    }
+
+    char key[256];
+    for (int i = 0; i < 1000; i++) {
+        bzero(key, 256);
+        sprintf(key, "%d", i);
+        cceph_mem_store_object_node *node = NULL;
+        int ret = cceph_mem_store_object_node_search(&root, key, &node, 0);
+        EXPECT_EQ(CCEPH_OK, ret);
+        EXPECT_NE((cceph_mem_store_object_node*)NULL, node);
+        EXPECT_STREQ(key, node->oid);
+    }
+}
