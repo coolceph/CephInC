baacccf7d09e526995c85077b2c5d0a8b45b0f41
Dong Yuan <yuandong1222@gmail.com>
2016-04-17 22:11:11 +0800

    MemStore: Finish unit test cceph_mem_store_object_node_new_and_free
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/test/test_os_mem_store_object_node.cc
+++ src/test/test_os_mem_store_object_node.cc
@@ -5,7 +5,24 @@ extern "C" {
 
 #include "gtest/gtest.h"
 
-TEST(os_mem_store, cceph_mem_store_object_node) {
+TEST(os_mem_store, cceph_mem_store_object_node_new_and_free) {
+    const char* oid    = "object";
+    int64_t     log_id = 122;
+
+    cceph_mem_store_object_node *node = NULL;
+    int ret = cceph_mem_store_object_node_new(oid, &node, log_id);
+    EXPECT_EQ(CCEPH_OK, ret);
+    EXPECT_NE((cceph_mem_store_object_node*)NULL, node);
+    EXPECT_STREQ(oid, node->oid);
+    EXPECT_EQ(NULL, node->data);
+    EXPECT_EQ(0, node->length);
+
+    ret = cceph_mem_store_object_node_free(&node, log_id);
+    EXPECT_EQ(CCEPH_OK, ret);
+    EXPECT_EQ((cceph_mem_store_object_node*)NULL, node);
+}
+
+TEST(os_mem_store, cceph_mem_store_object_node_insert_and_search) {
     cceph_rb_root root = CCEPH_RB_ROOT;
     char oid[256];
     for (int i = 0; i < 1000; i++) {
