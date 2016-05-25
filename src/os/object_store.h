581db7e4884711612ad3412728a086390787f6b0
Dong Yuan <yuandong1222@gmail.com>
2016-05-25 18:02:53 +0800

    OS: Add Definition of List Collection
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/os/object_store.h
+++ src/os/object_store.h
@@ -17,6 +17,12 @@ typedef int (*cceph_os_submit_tran_func)(
         cceph_os_tran*        tran,
         int64_t               log_id);
 
+typedef int (*cceph_os_list_coll_func)(
+        cceph_object_store*  os,
+        int64_t*             coll_id_list_length,
+        cceph_os_coll_id_t** coll_id_list,
+        int64_t              log_id);
+
 //if length <= 0 or length >= object->length, read the whole content
 typedef int (*cceph_os_read_obj_func)(
         cceph_object_store* os,
@@ -61,6 +67,8 @@ typedef int (*cceph_os_read_obj_map_key_func)(
 typedef struct {
     cceph_os_mount_func               mount;
     cceph_os_submit_tran_func         submit_tran;
+
+    cceph_os_list_coll_func           list_coll;
 
     cceph_os_read_coll_map_func       read_coll_map;
     cceph_os_read_coll_map_key_func   read_coll_map_key;
