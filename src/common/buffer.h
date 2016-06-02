147a178174ec9f64ecaebf2ec41cb862ae043545
Dong Yuan <yuandong1222@gmail.com>
2016-06-02 14:47:45 +0800

    COMMON: Add cceph_buffer_new
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/common/buffer.h
+++ src/common/buffer.h
@@ -1,10 +1,12 @@
 #ifndef CCEPH_BUFFER_H
 #define CCEPH_BUFFER_H
 
+#include "common/types.h"
+
 typedef struct cceph_buffer_node_ cceph_buffer_node_;
 struct cceph_buffer_node_ {
     char*   data;
-    char*   ptr;
+    char*   ptr;     //Point to current write position
     int32_t length;
 
     cceph_buffer_node_ *next;
@@ -15,8 +17,13 @@ typedef struct {
     cceph_buffer_node* head;
     cceph_buffer_node* current;
 
-    int32_t data_length;
+    int32_t length;
+    int32_t offset;
 } cceph_buffer;
+
+extern int cceph_buffer_new(
+        cceph_buffer** buffer,
+        int64_t        log_id);
 
 
 #endif
