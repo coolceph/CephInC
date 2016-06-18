4f57898526a65739b3c39178b4762fa98af9df62
Dong Yuan <yuandong1222@gmail.com>
2016-06-18 16:45:01 +0800

    COMMON: Add Unit Test for Decode Type Methods
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/test/test_common_encode.cc
+++ src/test/test_common_encode.cc
@@ -22,3 +22,23 @@ TEST(encode, type) {
     memcpy(&result, node->data, sizeof(result));
     EXPECT_EQ(value, result);
 }
+TEST(decode, type) {
+    cceph_buffer*        buffer = NULL;
+    cceph_buffer_reader* reader = NULL;
+    int32_t value  = 234;
+    int32_t result = 0;
+    int64_t log_id = 122;
+
+    int ret = cceph_buffer_new(&buffer, log_id);
+    EXPECT_EQ(CCEPH_OK, ret);
+
+    ret = cceph_encode_int32_t(buffer, value, log_id);
+    EXPECT_EQ(CCEPH_OK, ret);
+
+    ret = cceph_buffer_reader_new(&reader, buffer, log_id);
+    EXPECT_EQ(CCEPH_OK, ret);
+
+    ret = cceph_decode_int32_t(reader, &result, log_id);
+    EXPECT_EQ(CCEPH_OK, ret);
+    EXPECT_EQ(value, result);
+}
