479e03f2e0dd34106ec77b052932ff5eeada5d21
Dong Yuan <yuandong1222@gmail.com>
2016-06-28 15:08:33 +0800

    Common: Finish unit test for osdmap encode/decode
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/test/test_common_osdmap.cc
+++ src/test/test_common_osdmap.cc
@@ -29,3 +29,36 @@ TEST(cceph_osd_entity, encode_and_decode) {
     EXPECT_EQ(CCEPH_OK, ret);
     EXPECT_EQ(value.id, result.id);
 }
+TEST(cceph_osdmap, encode_and_decode) {
+    cceph_buffer*        buffer = NULL;
+    cceph_buffer_reader* reader = NULL;
+    cceph_osdmap value;
+    cceph_osdmap result;
+    cceph_osd_entity osds[3];
+    int64_t log_id = 122;
+
+    value.epoch     = 1;
+    value.pg_count  = 256;
+    value.osd_count = 3;
+    value.osds      = &osds[0];
+    osds[0].id      = 0;
+    osds[1].id      = 1;
+    osds[2].id      = 2;
+
+    int ret = cceph_buffer_new(&buffer, log_id);
+    EXPECT_EQ(CCEPH_OK, ret);
+    ret = cceph_encode_osdmap(buffer, &value, log_id);
+    EXPECT_EQ(CCEPH_OK, ret);
+
+    ret = cceph_buffer_reader_new(&reader, buffer, log_id);
+    EXPECT_EQ(CCEPH_OK, ret);
+    ret = cceph_decode_osdmap(reader, &result, log_id);
+    EXPECT_EQ(CCEPH_OK, ret);
+
+    EXPECT_EQ(value.epoch, result.epoch);
+    EXPECT_EQ(value.pg_count, result.pg_count);
+    EXPECT_EQ(value.osd_count, result.osd_count);
+    for (int i = 0; i < value.osd_count; i++) {
+        EXPECT_EQ(value.osds[i].id, result.osds[i].id);
+    }
+}
