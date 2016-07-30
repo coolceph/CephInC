940d5b4667b37677f9688b85f57f658e8a48473b
Dong Yuan <yuandong1222@gmail.com>
2016-07-30 11:21:03 +0800

    Common: Finish Unit Test of Util intcmp
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/common/util.c
+++ src/common/util.c
@@ -2,9 +2,9 @@
 
 int intcmp(int a, int b) {
     if (a > b) {
-        return -1;
-    } else if (a < b) {
         return 1;
+    } else if (a < b) {
+        return -1;
     } else {
         return 0;
     }
