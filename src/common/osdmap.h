bd8710e7cc1276739a41b5323de8f7b9a8b0e44a
Dong Yuan <yuandong1222@gmail.com>
2016-08-02 10:27:02 +0800

    COMMON: Add Map Methods of OSDMap
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- src/common/osdmap.h
+++ src/common/osdmap.h
@@ -3,6 +3,7 @@
 
 #include "common/buffer.h"
 #include "common/encode.h"
+#include "common/rbtree.h"
 
 typedef struct {
     cceph_osd_id_t id;
@@ -19,9 +20,26 @@ typedef struct {
     int osd_count;
     cceph_osd_entity* osds;
 
+    cceph_rb_node node;
 } cceph_osdmap;
 
 CCEPH_DEFINE_ENCODE_METHOD(osd_entity);
 CCEPH_DEFINE_ENCODE_METHOD(osdmap);
+
+extern int cceph_osdmap_map_insert(
+        cceph_rb_root *root,
+        cceph_osdmap  *node,
+        int64_t       log_id);
+
+extern int cceph_osdmap_map_remove(
+        cceph_rb_root *root,
+        cceph_osdmap  *node,
+        int64_t       log_id);
+
+extern int cceph_osdmap_map_search(
+        cceph_rb_root *root,
+        cceph_epoch_t key,
+        cceph_osdmap  **result,
+        int64_t       log_id);
 
 #endif
